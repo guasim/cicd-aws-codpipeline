4b61f0c8cd2de4273f41cc960f0af60ee96c9683
Al-Waseem <guasim.dev@gmail.com>
2023-04-23 17:38:50 +0200

    first commit

---
+++ src/main/java/com/guasim/dev/ms/CicdAwsCodePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.guasim.dev.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAwsCodePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsCodePipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guasim/dev/ms/CicdAwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guasim.dev.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdAwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
